f3a5ae33ceb326405a3fbec84d19f9d7b431c012
Kokoster <dokokoster9@gmail.com>
2016-08-29 15:48:43 +0300

    fixed problems with creating detail activity

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine.app;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
